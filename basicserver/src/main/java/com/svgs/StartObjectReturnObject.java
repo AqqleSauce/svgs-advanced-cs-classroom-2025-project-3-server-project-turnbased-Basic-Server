68bfcef1cce6a73e0d15908f9befecc1da01553a
AqqleSauce <alexisgusfab@gmail.com>
2026-02-06 10:50:39 -0500

    commit and sync it part 2

--- basicserver/src/main/java/com/svgs/StartObjectReturnObject.java
+++
@@ -1,9 +0,0 @@
-package com.svgs;
-
-public class StartObjectReturnObject {
-    Player[] playerReturnList;
-
-    public StartObjectReturnObject(int listSize){
-        playerReturnList = new Player[listSize];
-    }
-}
